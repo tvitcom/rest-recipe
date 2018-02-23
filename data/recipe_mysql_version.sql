884229117faafc0c6cb677f0fc749462be78b65a
Roman <tvitcom@yandex.ru>
2018-02-23 07:16:55 +0200

    postgre datastorage and entities is ready

---
+++ data/recipe_mysql_version.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Фев 23 2018 г., 05:14
+-- Версия сервера: 5.5.59-0+deb8u1
+-- Версия PHP: 5.6.33-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `recipe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `author`
+--
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` bigint(21) unsigned NOT NULL,
+  `email` varchar(63) NOT NULL,
+  `login` varchar(31) NOT NULL,
+  `pass_hash` varchar(127) NOT NULL,
+  `api_key` varchar(127) DEFAULT NULL,
+  `ts_create` int(11) NOT NULL,
+  `ts_update` int(11) DEFAULT NULL,
+  `recover_key` varchar(127) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `recipe`
+--
+
+CREATE TABLE IF NOT EXISTS `recipe` (
+  `id` bigint(21) unsigned NOT NULL,
+  `author_id` bigint(21) unsigned NOT NULL,
+  `ts_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `title` varchar(55) NOT NULL,
+  `content` text,
+  `picture_uri` varchar(255) DEFAULT NULL,
+  `is_enable` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `recipe`
+--
+ALTER TABLE `recipe`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_recipe_author_idx` (`author_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `author`
+--
+ALTER TABLE `author`
+  MODIFY `id` bigint(21) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `recipe`
+--
+ALTER TABLE `recipe`
+  MODIFY `id` bigint(21) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `recipe`
+--
+ALTER TABLE `recipe`
+  ADD CONSTRAINT `fk_recipe_author` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
