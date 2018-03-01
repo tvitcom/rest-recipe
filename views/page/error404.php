85d5b1829a7365a44853e0cbecffcc221e17b048
Roman <tvitcom@yandex.ru>
2018-03-01 04:41:21 +0200

    replace HTML head to main_header template

--- views/page/error404.php
+++ views/page/error404.php
@@ -4,27 +4,6 @@
  * Template Copyright 2018 by https://github.com/verekia/.
  */
 ?>
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-	<title>Error 404 Page not found</title>
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-
-	<link rel="stylesheet" href="/static/css/style.css">
-
-	<script src="/static/js/libs/modernizr-2.0.6.min.js"></script>
-</head>
-<body>
-
 <?php require 'main_header.php'?>
 	<div id="main-container">
 		<div id="main" class="wrapper clearfix">
